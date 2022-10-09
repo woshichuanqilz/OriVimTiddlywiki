358106b54949b14feec98c9a8fd7c80478ec8a1c
Ori <imlegendlzz@gmail.com>
2022-10-09 17:42:02 +0800

    update

--- lua/orivim.lua
+++ lua/orivim.lua
@@ -1,7 +1,17 @@
 local M = {}
+local keymap = vim.keymap.set
+local default_opts = { noremap = true, silent = true }
+
+function M.search_tid()
+    print("Hello from orivim")
+    require('telescope.builtin').find_files { shorten_path = true, cwd = "/home/lizhe/OriNote/OriWiki/tiddlers/" }
+end
+
 
 function M.setup()
-    print("Hello from orivim")
+    vim.api.nvim_set_keymap('n', 'x', 'C', {noremap = true})
+    -- keymap('n', 'x', 'C', default_opts)
 end
+
 
 return M
