44a1ec5552d6595c858c61c2bd6db3cc67530079
Ori <imlegendlzz@gmail.com>
2023-01-07 16:10:59 +0800

    update

--- lua/orivim.lua
+++ lua/orivim.lua
@@ -1,14 +1,16 @@
 local M = {}
 local keymap = vim.keymap.set
 local default_opts = { noremap = true, silent = true }
+local lvim = require('lvim').lvim
 
 function M.search_note()
-    print("Hello from orivim")
     require('telescope.builtin').find_files { shorten_path = true, cwd = "/home/lizhe/OriNote/OriWiki/tiddlers/" }
+    -- lvim.builtin.which_key.mappings["n"] = { ":lua require('telescope.builtin').find_files { shorten_path = true, cwd = '/home/lizhe/OriNote/notes/' }<CR>", "Run current script", silent = true }
 end
 
 function M.keymap_setup()
     print("keymap setup")
+    lvim.builtin.which_key.mappings["n"] = { ":lua require('telescope.builtin').find_files { shorten_path = true, cwd = '/home/lizhe/OriNote/notes/' }<CR>", "Run current script", silent = true }
 end
 
 function M.create_note(note_name)
