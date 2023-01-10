42f83cdbcaf7f2b44b2aba7c0030ca55bd4574d8
Ori <imlegendlzz@gmail.com>
2023-01-10 18:42:50 +0800

    update

--- lua/orivim/mappings.lua
+++ lua/orivim/mappings.lua
@@ -4,7 +4,7 @@ local keymap = vim.keymap.set
 local default_opts = { noremap = true, silent = true }
 
 function M.keymap_setup()
-  vim.api.nvim_set_keymap('n', 'x', 'C', {noremap = true})
+  -- vim.api.nvim_set_keymap('n', 'x', 'C', {noremap = true})
   keymap({'n', 'v'}, 'gd', '<cmd>OpenNoteUnderCursor<CR>', default_opts)
 end
 
