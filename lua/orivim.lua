023e843b906782e4ad72697e8dec2ec9e170b167
Ori <imlegendlzz@gmail.com>
2023-01-12 20:36:21 +0800

    update

--- lua/orivim.lua
+++ lua/orivim.lua
@@ -1,4 +1,6 @@
-local keymap = require('orivim.mappings')
+-- local keymap = require('orivim.mappings')
+local keymap = vim.keymap.set
+local default_opts = { noremap = true, silent = true }
 
 local M = {}
 -- local lvim = require('lvim').lvim
@@ -79,11 +81,20 @@ function M.set_autoCommand()
 end
 
 
+function M.keymap_setup(note_path)
+  keymap({'n', 'v'}, 'gd', '<cmd>OpenNoteUnderCursor<CR>', default_opts)
+  keymap({'n', 'v'}, '<A-q>', '<cmd>execute "normal \\<Plug>Markdown_OpenUrlUnderCursor"<CR>', default_opts)
+  vim.cmd('nnoremap <leader>t :only<CR>:Toc<CR>')
+  keymap({'n', 'v'}, '<A-f>', ":lua require('telescope.builtin').live_grep({cwd='" .. note_path ..  "'})<CR>", default_opts)
+  keymap({'n', 'v'}, '<A-i>', "", default_opts)
+end
+
+
 function M.setup()
   vim.api.nvim_create_autocmd("BufEnter", {
     pattern = { "*.md" },
     callback = function ()
-      keymap.keymap_setup(M.note_path)
+      M.keymap_setup(M.note_path)
       M.set_autoCommand()
     end,
   })
--- lua/orivim/mappings.lua
+++ lua/orivim/mappings.lua
@@ -1,16 +1,2 @@
 -- mappings
-require('file_name_picker')
 
-local M = {}
-local keymap = vim.keymap.set
-local default_opts = { noremap = true, silent = true }
-
-function M.keymap_setup(note_path)
-  keymap({'n', 'v'}, 'gd', '<cmd>OpenNoteUnderCursor<CR>', default_opts)
-  keymap({'n', 'v'}, '<A-q>', '<cmd>execute "normal \\<Plug>Markdown_OpenUrlUnderCursor"<CR>', default_opts)
-  vim.cmd('nnoremap <leader>t :only<CR>:Toc<CR>')
-  keymap({'n', 'v'}, '<A-f>', ":lua require('telescope.builtin').live_grep({cwd='" .. note_path ..  "'})<CR>", default_opts)
-  keymap({'n', 'v'}, '<A-i>', "", default_opts)
-end
-
-return M
