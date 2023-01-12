257af1d02394ac16e8d999b01e1681c9de9b32bd
Ori <imlegendlzz@gmail.com>
2023-01-12 09:59:44 +0800

    update

--- lua/orivim/mappings.lua
+++ lua/orivim/mappings.lua
@@ -6,7 +6,7 @@ local default_opts = { noremap = true, silent = true }
 function M.keymap_setup()
   -- vim.api.nvim_set_keymap('n', 'x', 'C', {noremap = true})
   keymap({'n', 'v'}, 'gd', '<cmd>OpenNoteUnderCursor<CR>', default_opts)
-  keymap({'n', 'v'}, '<A-q>', '<cmd>execute "normal \<Plug>OpenNoteUnderCursor"<CR>', default_opts)
+  keymap({'n', 'v'}, '<A-q>', '<cmd>execute "normal \\<Plug>Markdown_OpenUrlUnderCursor"<CR>', default_opts)
 end
 
 return M
