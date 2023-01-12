5a6749bbfeb74369d035ceb7d284efacb4597a08
Ori <imlegendlzz@gmail.com>
2023-01-12 10:46:48 +0800

    update

--- lua/orivim/mappings.lua
+++ lua/orivim/mappings.lua
@@ -4,12 +4,9 @@ local keymap = vim.keymap.set
 local default_opts = { noremap = true, silent = true }
 
 function M.keymap_setup(note_path)
-  -- vim.api.nvim_set_keymap('n', 'x', 'C', {noremap = true})
   keymap({'n', 'v'}, 'gd', '<cmd>OpenNoteUnderCursor<CR>', default_opts)
   keymap({'n', 'v'}, '<A-q>', '<cmd>execute "normal \\<Plug>Markdown_OpenUrlUnderCursor"<CR>', default_opts)
   vim.cmd('nnoremap <leader>t :only<CR>:Toc<CR>')
-  -- keymap({'n', 'v'}, '<A-f>', ":lua require('telescope.builtin').live_grep({cwd='" .. note_path ..  "}')", default_opts)
-  -- keymap({'n', 'v'}, '<A-f>', ":lua require('telescope.builtin').live_grep()<CR>", default_opts)
   keymap({'n', 'v'}, '<A-f>', ":lua require('telescope.builtin').live_grep({cwd='" .. note_path ..  "'})<CR>", default_opts)
 end
 
