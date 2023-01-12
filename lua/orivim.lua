abdd4bfe186411ed2a0194d541e6702afdd995b6
Ori <imlegendlzz@gmail.com>
2023-01-12 20:42:53 +0800

    update

--- lua/orivim.lua
+++ lua/orivim.lua
@@ -86,7 +86,7 @@ function M.keymap_setup(note_path)
   keymap({'n', 'v'}, '<A-q>', '<cmd>execute "normal \\<Plug>Markdown_OpenUrlUnderCursor"<CR>', default_opts)
   vim.cmd('nnoremap <leader>t :only<CR>:Toc<CR>')
   keymap({'n', 'v'}, '<A-f>', ":lua require('telescope.builtin').live_grep({cwd='" .. note_path ..  "'})<CR>", default_opts)
-  keymap({'n', 'v'}, '<A-i>', "<cmd>lua file_name_picker.insert_internal_link()<CR>", default_opts)
+  keymap({'n', 'v', 'i'}, '<A-i>', "<cmd>lua require('orivim.file_name_picker').insert_internal_link()<CR>", default_opts)
 end
 
 
@@ -103,3 +103,4 @@ end
 
 
 return M
+
