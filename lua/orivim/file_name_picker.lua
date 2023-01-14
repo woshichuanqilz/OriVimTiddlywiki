53624a5271103ec2b68548484e053b4072724d42
Ori <imlegendlzz@gmail.com>
2023-01-14 20:03:43 +0800

    update

--- lua/orivim/file_name_picker.lua
+++ lua/orivim/file_name_picker.lua
@@ -12,10 +12,42 @@ M.colors = function(opts)
   opts = opts or {}
   pickers.new(opts, {
     prompt_title = "Ori",
-    -- content of the list
-    -- finder = finders.new_table {
     --   results = { "red", "green", "blue" }
-    -- },
+    finder = finders.new_table {
+      results = {
+        { "red", "#ff0000" },
+        { "green", "#00ff00" },
+        { "blue", "#0000ff" },
+      },
+      entry_maker = function(entry)
+        -- print(vim.inspect(entry))
+        return {
+          value = entry,
+          display = entry[1],
+          ordinal = entry[1],
+        }
+      end
+    },
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        print(vim.inspect(selection))
+        vim.api.nvim_put({ selection[1] }, "", false, true)
+      end)
+      return true
+    end,
+  }):find()
+end
+
+
+-- our picker function: colors, 
+M.find_tags = function(opts)
+  opts = opts or {}
+  opts['cwd'] = '/home/lizhe/OriNote/notes/tags.txt'
+  pickers.new(opts, {
+    prompt_title = "Ori Tags",
     finder = finders.new_table {
       results = {
         { "red", "#ff0000" },
