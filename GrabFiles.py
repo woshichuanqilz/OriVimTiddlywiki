0048fa27846b17aef5ab8caa7ebdc5805ed23c82
Ori <imlegendlzz@gmail.com>
2023-01-16 01:26:43 +0800

    update

--- GrabFiles.py
+++ GrabFiles.py
@@ -6,11 +6,14 @@ import os
 def grabFiles(path):
     for root, dirs, files in os.walk(path):
         for file in files:
-            print(os.path.join(root, file))
+            path = os.path.join(root, file)
 
 
 if __name__ == '__main__':
     # folder from argument
     import sys
-
-    grabFiles(sys.argv[1])
+    if len(sys.argv) < 2:
+        path = '/home/lizhe/OriNote/notes/'
+    else:
+        path = sys.argv[1]
+    grabFiles(path)
